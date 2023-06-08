2372fefec27414989554faf98787317644ffeef5
Dongwon Kang <dw3624@gmail.com>
2023-06-08 14:47:18 +0900

    Feat: 배포 완료

--- dash/gunicorn_config.py
+++ dash/gunicorn_config.py
@@ -1,6 +1,6 @@
 bind = "0.0.0.0:8050"  # 바인딩할 IP 및 포트
 workers = 4  # 워커(worker) 프로세스 수
-timeout = 60  # 요청 타임아웃 (초)
+timeout = 300  # 요청 타임아웃 (초)
 
 # 로깅 설정
 accesslog = "/var/log/gunicorn_access.log"  # 액세스 로그 파일 경로
