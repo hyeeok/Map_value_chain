386393c5543505a07577f634c8cbb681f4058e1d
hyeeok <dlgur57@gmail.com>
2023-10-18 15:11:50 +0900

    feat: 프로젝트 생성

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.mvc.routers import router as mvc_router
+
+app = FastAPI()
+
+# 허용할 오리진(출처) 목록
+origins = ["http://127.0.0.1:3000", "http://localhost:3000","http://localhost","http://127.0.0.1"]
+
+# CORS 미들웨어 추가해 오리진 허용
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # 허용할 출처 목록
+    allow_credentials=True, # 자격 증명 허용 여부
+    allow_methods=["*"],  # 허용할 HTTP 메서드
+    allow_headers=["*"],  # 허용할 HTTP 헤더
+)
+
+app.include_router(mvc_router)
