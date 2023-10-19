627f2fcacd6fee3982c42747d756a9c25e1fe816
Dongwon Kang <dw3624@gmail.com>
2023-10-19 16:49:44 +0900

    docs: modify requirements.txt

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,10 @@
-from os.path import join, dirname
+import os
+from os.path import dirname, join
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-import os
 
 load_dotenv()
 DB_USER=os.environ.get("DB_USER")
@@ -12,8 +12,6 @@ DB_PASS=os.environ.get("DB_PASS")
 DB_HOST=os.environ.get("DB_HOST")
 DB_NAME=os.environ.get("DB_NAME")
 
-# postgresql+psycopg2 : SQLAlchemy에서 PostgreSQL db에 연결할 때 사용하는 URL 스키마
-# SQLAlchemy는 기본적으로 psycopg2를 사용하기 때문에 따로 안 적어도 됨
 SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}/{}".format(
     DB_USER,
     DB_PASS,
