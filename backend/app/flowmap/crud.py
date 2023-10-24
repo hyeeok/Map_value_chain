8672897f53a7a1c01fab136560c93e12c59d4d9b
hyeeok <dlgur57@gmail.com>
2023-10-24 15:25:19 +0900

    update_flowmap api

--- backend/app/flowmap/crud.py
+++ backend/app/flowmap/crud.py
@@ -2,6 +2,8 @@ import json
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 
+from app.flowmap.schemas import FlowmapBase
+
 
 def get_industry_class_list(db: Session):
     query = text(
@@ -21,10 +23,15 @@ def get_flowmap(db: Session):
     return {"node": result[0][0], "edge": result[0][1]}
 
 
-# def set_flowmap(db: Session, new_node: json, new_edge: json, domainId: int):
-#     query = text(
-#         "UPDATE flowmap SET node = :new_node, edge = :new_edge WHERE domain_id = :domainId;"
-#     )
+def set_flowmap(db: Session, new_data: FlowmapBase):
+    new_node_json = json.dumps(new_data.node)
+    new_edge_json = json.dumps(new_data.edge)
+
+    query = text("UPDATE flowmap SET node = :new_node, edge = :new_edge;")
+    db.execute(query, {"new_node": new_node_json, "new_edge": new_edge_json})
+    db.commit()
+
+    return {"node": new_data.node, "edge": new_data.edge}
 
 
 # def get_industry_class_flowmap(db: Session, industryClassCode: int):
--- backend/app/flowmap/routers.py
+++ backend/app/flowmap/routers.py
@@ -9,6 +9,8 @@ from . import crud
 from .models import *
 from .schemas import *
 
+import json
+
 router = APIRouter(prefix="/flowmap")
 
 
@@ -22,4 +24,10 @@ def read_industry_class_list(db: Session = Depends(get_db)):
 @router.get("", response_model=FlowmapBase)
 def read_flowmap(db: Session = Depends(get_db)):
     result = crud.get_flowmap(db)
+    return result
+
+
+@router.put("", response_model=FlowmapBase)
+def update_flowmap(new_data: FlowmapBase, db: Session = Depends(get_db)):
+    result = crud.set_flowmap(db, new_data)
     return result
