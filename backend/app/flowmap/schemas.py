e6f3447e396bd53ac05cbb3a9a58cdd7be434a80
Dongwon Kang <dw3624@gmail.com>
2023-10-26 11:49:33 +0900

    feat: add domain api

--- backend/app/flowmap/schemas.py
+++ backend/app/flowmap/schemas.py
@@ -1,7 +1,13 @@
 from typing import Any, Dict, List, Optional, Tuple, Union
 
-import stringcase
-from pydantic import BaseModel, Json
+from app.utils import to_camel
+from pydantic import BaseModel, Field, Json
+
+
+class CamelModel(BaseModel):
+    class config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
 
 
 class IndustryClassBase(BaseModel):
@@ -39,4 +45,37 @@ class FlowmapBase(BaseModel):
 class Flowmap(FlowmapBase):
     class Config:
         form_attributes = True
-        stringcase.camelcase = True
+
+
+class DomainIndustryClass(BaseModel):
+    industryClassId: int = Field(..., alias="id")
+    industryClassCode: str = Field(..., alias="code")
+    industryClassName: str = Field(..., alias="name")
+
+    class Config:
+        from_attributes = True
+
+
+class DomainBase(BaseModel):
+    domainId: int = Field(..., alias="id")
+    domainCode: str = Field(..., alias="code")
+    domainName: str = Field(..., alias="name")
+
+    class Config:
+        from_attributes = True
+
+
+class Domain(DomainBase):
+    classes: Optional[List[DomainIndustryClass]] = []
+    themes: Optional[List[DomainIndustryClass]] = []
+
+    class Config:
+        from_attributes = True
+
+
+class DomainList(CamelModel):
+    length: int
+    data: List[Union[Domain, None]]
+
+    class Config:
+        from_attributes = True
