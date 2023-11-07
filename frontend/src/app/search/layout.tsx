5df13bf7daadcd293026f5e0751dbd7698371f97
dongwon kang <dw3624@gmail.com>
2023-11-07 19:00:36 +0900

    feat: separate layout

---
+++ frontend/src/app/search/layout.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Sidenav from '@/app/search/_components/sidenav';
+import { SIDENAV } from '@/app/search/_const/test';
+// import SearchResult from '@/app/search/_components/search-result';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const SearchLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="container flex flex-col px-8">
+      <section className="flex flex-1 gap-6">
+        <Sidenav sidenav={SIDENAV} />
+        <div className="flex-1">
+          <section className="pt-6 pb-4">
+            <div className="flex gap-2">
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="회사명" defaultValue="corpName" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="corpName">회사명</SelectItem>
+                  <SelectItem value="copCode">사업자등록번호</SelectItem>
+                  <SelectItem value="regCode">법인등록번호</SelectItem>
+                  <SelectItem value="stockCode">증권종목코드</SelectItem>
+                </SelectContent>
+              </Select>
+              <Input />
+              <Button className="w-[200px]">검색</Button>
+            </div>
+          </section>
+          <div className="flex-1 pb-6">{children}</div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SearchLayout;
--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,49 +1,18 @@
 import React from 'react';
 
-import IndustryInfo from '@/app/search/_components/industry-info';
-import Sidenav from '@/app/search/_components/sidenav';
-import { SIDENAV } from '@/app/search/_const/test';
-// import SearchResult from '@/app/search/_components/search-result';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
+// import IndustryInfo from '@/app/search/_components/industry-info';
+import SearchResult from '@/app/search/_components/search-result';
 
-const sidenavTest = SIDENAV;
+// 진입시 query 존재 여부 확인
+// query 없는 경우 빈화면 표시
+// query 있는 경우 get
+// 검색 -> ?name=blah로 이동 -> page 표시 -> query에서 blah 추출 -> 데이터 표시
+
 const SearchPage = () => {
   return (
-    <div className="container flex flex-col px-8">
-      <section className="flex flex-1 gap-6">
-        <Sidenav sidenav={sidenavTest} />
-        <div className="flex-1">
-          <section className="py-6">
-            <div className="flex gap-2">
-              <Select>
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="회사명" defaultValue="corpName" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="corpName">회사명</SelectItem>
-                  <SelectItem value="copCode">사업자등록번호</SelectItem>
-                  <SelectItem value="regCode">법인등록번호</SelectItem>
-                  <SelectItem value="stockCode">증권종목코드</SelectItem>
-                </SelectContent>
-              </Select>
-              <Input />
-              <Button className="w-[200px]">검색</Button>
-            </div>
-          </section>
-          <div className="flex-1 pb-6">
-            {/* <SearchResult /> */}
-            <IndustryInfo />
-          </div>
-        </div>
-      </section>
+    <div className="flex flex-col">
+      <SearchResult />
+      {/* <IndustryInfo /> */}
     </div>
   );
 };
