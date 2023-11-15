de08fe80a99c9873b88f875f04e0cb715d431b76
dongwon kang <dw3624@gmail.com>
2023-11-15 16:02:52 +0900

    test: add sidenav test data

--- frontend/src/app/search/_const/test.ts
+++ frontend/src/app/search/_const/test.ts
@@ -13,35 +13,47 @@ export const SIDENAV = {
       industryList: [{ industryName: '산업1' }, { industryName: '산업2' }],
     },
   ],
-  industryTab: [
+
+  industryData: {},
+};
+
+export const SIDENAV_DATA = {
+  data: [
     {
-      mainCategoryName: '대분류',
-      middleCategoryList: [
-        {
-          middleCategoryName: '중분류',
-          smallCategoryList: [
-            { smallCategoryName: '소분류1' },
-            { smallCategoryName: '소분류2' },
-            { smallCategoryName: '소분류3' },
-          ],
-        },
-        {
-          middleCategoryName: '중분류',
-          smallCategoryList: [
-            { smallCategoryName: '소분류1' },
-            { smallCategoryName: '소분류2' },
-          ],
-        },
-      ],
+      domainCode: 'IH',
+      domainName: 'ICT HW',
+      industryCode: 'IHCSC',
+      industryName: '반도체/부품/장비',
+      className: 'WAFER & MASK',
+      classCode: 'A',
+      classLevel: 1,
     },
     {
-      mainCategoryName: '대분류',
-      middleCategoryList: [
-        {
-          middleCategoryName: '중분류',
-          smallCategoryList: [{ smallCategoryName: '소분류1' }],
-        },
-      ],
+      domainCode: 'IH',
+      domainName: 'ICT HW',
+      industryCode: 'IHCSC',
+      industryName: '반도체/부품/장비',
+      className: 'FABRICATION',
+      classCode: 'A10',
+      classLevel: 2,
+    },
+    {
+      domainCode: 'IH',
+      domainName: 'ICT HW',
+      industryCode: 'IHCSC',
+      industryName: '반도체/부품/장비',
+      className: 'ASSEMBLY & TEST',
+      classCode: 'A20',
+      classLevel: 2,
+    },
+    {
+      domainCode: 'IH',
+      domainName: 'ICT HW',
+      industryCode: 'IHCSC',
+      industryName: '반도체/부품/장비',
+      className: 'Discrete',
+      classCode: 'A10010',
+      classLevel: 3,
     },
   ],
 };
--- frontend/src/app/search/layout.tsx
+++ frontend/src/app/search/layout.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import SearchBox from '@/app/search/_components/search-box';
 import Sidenav from '@/app/search/_components/sidenav';
-import { SIDENAV } from '@/app/search/_const/test';
+import { SIDENAV_DATA } from '@/app/search/_const/test';
 
 const SearchLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="container flex flex-col px-8">
       <section className="flex flex-1 gap-6">
-        <Sidenav sidenav={SIDENAV} />
+        <Sidenav sidenav={SIDENAV_DATA} />
         <div className="flex-1">
           <section className="pt-6 pb-4">
             <SearchBox />
