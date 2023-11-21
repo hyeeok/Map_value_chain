08f1a7f48a5cc4c0f9f3940b7dafe34b3cf3fdbb
Dongwon Kang <dw3624@gmail.com>
2023-11-22 08:19:10 +0900

    feat: change structure

--- frontend/src/app/overview/_components/overview-list.tsx
+++ frontend/src/app/overview/_components/overview-list.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
+import page from '@/app/page';
 import {
   Table,
   TableBody,
@@ -31,54 +32,59 @@ interface OverviewListType {
 const OverviewList = ({ datas }: { datas: OverviewListType[] }) => {
   const router = useRouter();
   return (
-    <div className="rounded-md">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>회사명</TableHead>
-            <TableHead>사업자등록번호</TableHead>
-            <TableHead>법인구분</TableHead>
-            <TableHead>종목코드</TableHead>
-            <TableHead>계열모회사</TableHead>
-            <TableHead>대표자명</TableHead>
-            <TableHead>설립일</TableHead>
-            <TableHead>지역</TableHead>
-            <TableHead>홈페이지</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {datas.map((data, i) => (
-            <TableRow key={i}>
-              <TableCell>
-                <Link
-                  href={`/overview/${data.corpCode}`}
-                  className="hover:underline"
-                >
-                  {data.firmName}
-                </Link>
-              </TableCell>
-              <TableCell>{data.bizrNo}</TableCell>
-              <TableCell>{data.jurirNo}</TableCell>
-              <TableCell>{data.stockCode}</TableCell>
-              <TableCell>{data.conglomerateName || '-'}</TableCell>
-              <TableCell>{data.ceoName}</TableCell>
-              <TableCell>{data.establishDate}</TableCell>
-              <TableCell>
-                {data.adress1} {data.adress2}
-              </TableCell>
-              <TableCell>
-                {data.homepage ? (
-                  <Link href={data.homepage} className="hover:underline">
-                    바로가기
-                  </Link>
-                ) : (
-                  <>-</>
-                )}
-              </TableCell>
+    <div className="flex flex-col h-full justify-between">
+      <div className="rounded-md">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>회사명</TableHead>
+              <TableHead>사업자등록번호</TableHead>
+              <TableHead>법인구분</TableHead>
+              <TableHead>종목코드</TableHead>
+              <TableHead>계열모회사</TableHead>
+              <TableHead>대표자명</TableHead>
+              <TableHead>설립일</TableHead>
+              <TableHead>지역</TableHead>
+              <TableHead>홈페이지</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {datas.map((data, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <Link
+                    href={`/overview/${data.corpCode}`}
+                    className="hover:underline"
+                  >
+                    {data.firmName}
+                  </Link>
+                </TableCell>
+                <TableCell>{data.bizrNo}</TableCell>
+                <TableCell>{data.jurirNo}</TableCell>
+                <TableCell>{data.stockCode}</TableCell>
+                <TableCell>{data.conglomerateName || '-'}</TableCell>
+                <TableCell>{data.ceoName}</TableCell>
+                <TableCell>{data.establishDate}</TableCell>
+                <TableCell>
+                  {data.adress1} {data.adress2}
+                </TableCell>
+                <TableCell>
+                  {data.homepage ? (
+                    <Link href={data.homepage} className="hover:underline">
+                      바로가기
+                    </Link>
+                  ) : (
+                    <>-</>
+                  )}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex justify-center pt-6">
+        <Pagination currentPage={page} pageNum={Math.ceil(length / limit)} />
+      </div>
     </div>
   );
 };
--- frontend/src/app/overview/page.tsx
+++ frontend/src/app/overview/page.tsx
@@ -1,11 +1,26 @@
+import { cookies } from 'next/headers';
 import React from 'react';
 
-import SearchResult from '@/app/overview/_components/search-result';
+import { baseUrl } from '@/api/api-client';
+import OverviewList from '@/app/overview/_components/overview-list';
 
-const OverviewPage = () => {
+const getOverviewList = async () => {
+  const cookieStore = cookies();
+  try {
+    const response = await fetch(`${baseUrl}/overview`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const OverviewPage = async () => {
+  const overviewListData = await getOverviewList();
+
   return (
-    <div className="flex flex-col">
-      <SearchResult />
+    <div>
+      <OverviewList data={overviewListData} />
     </div>
   );
 };
