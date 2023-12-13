d890aaa1f6d83abeb3e00510337bfe89ef3b0667
dongwon kang <dw3624@gmail.com>
2023-12-13 12:59:19 +0900

    feat: add home

--- frontend/src/app/home/_components/vc-map.tsx
+++ frontend/src/app/home/_components/vc-map.tsx
@@ -1,9 +1,7 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-
 export type VCMapElementType = {
   domainCode: string;
   domainName: string;
-  domainDivision: number;
+  domainDivision: string;
   classList: {
     industryClassCode: string;
     industryClassName: string;
@@ -28,50 +26,50 @@ const VCMap = ({ data }: { data: VCMapElementType[] }) => {
   return (
     <div className="flex flex-wrap gap-2">
       {data.map((domainItem, di) => (
-        <Card
+        <div
           key={di}
-          className={`flex-auto box-border ${
+          className={`flex flex-col px-6 py-4 flex-auto box-border ${
             divisionColor[domainItem.domainDivision]
           }`}
         >
-          <CardHeader>
-            <CardTitle className="inline-block flex justify-between text-primary-foreground">
+          <div>
+            <div className="inline-block flex justify-between text-primary-foreground">
               {domainItem.domainName}
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
+            </div>
+          </div>
+          <div>
             <div>
-              <label className="text-sm font-medium leading-none mb-2 text-primary-foreground">
+              <label className="text-xs font-medium leading-none mb-2 text-primary-foreground">
                 Classes
               </label>
               <div className="flex flex-wrap gap-2">
                 {domainItem.classList.map((classItem, ci) => (
                   <div
                     key={ci}
-                    className="px-4 py-2 border text-sm text-primary-foreground"
+                    className="px-2.5 py-1.5 border text-xs text-primary-foreground"
                   >
                     {classItem.industryClassName}
                   </div>
                 ))}
               </div>
             </div>
-            <div className="pt-2">
-              <label className="text-sm font-medium leading-none mb-2 text-primary-foreground">
+            <div>
+              <label className="text-xs font-medium leading-none mb-2 text-primary-foreground">
                 Themes
               </label>
               <div className="flex flex-wrap gap-2">
                 {domainItem.themeList.map((themeItem, ti) => (
                   <div
                     key={ti}
-                    className="px-4 py-2 border text-sm text-primary-foreground"
+                    className="px-2 py-1 border text-xs text-primary-foreground"
                   >
                     {themeItem.industryClassName}
                   </div>
                 ))}
               </div>
             </div>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       ))}
     </div>
   );
